4f3471023822199c2485910113015ee584b5d284
Android <kanybekoov@gmail.com>
2021-02-15 23:02:21 +0600

    Request

--- app/src/main/java/com/example/todosonline/TodoApi.kt
+++ app/src/main/java/com/example/todosonline/TodoApi.kt
@@ -1,5 +1,11 @@
 package com.example.todosonline
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.POST
+
 interface TodoApi {
-    
+    @GET("/todos")
+    suspend fun getTodos(): Response<List<Todo>>
+
 }
